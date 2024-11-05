6ad2f786a6a44e371a67ad6aefa7f5cd3a2c52af
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2024-11-05 19:57:09 +0400

    section 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
+import Section1 from './components/Section1/Section1'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className='app'>
       <Navbar />
       <Header />
+      <Section1 />
       <Footer />
     </div>
   )
---
+++ src/components/Section1/Section1.jsx
@@ -0,0 +1,67 @@
+import './Section1.css'
+
+function Section1() {
+  return (
+    <div className='section1'>
+      <h1 className="sec1-header">
+        Gələcəyini necə inşa etmək istəyirsən?
+      </h1>
+      <div className="sec1-fields">
+        <div className="field">
+          <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+          <h2>Fullstack Kadr Yetişdirmə Proqramı</h2>
+          <p>HTML, CSS, Tailwind, JS, React, NextJS, NodeJS, MongoDB, XML, API, Git, Deployment</p>
+          </div>
+        </div>
+        <div className="field">
+        <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+          <h2>Digital Marketing Kadr Yetişdirmə Proqramı</h2>
+          <p>Marketinqə giriş, Rəqəmsal marketinq, SMM, Kontent ilə işləmə, Google Ads və s.</p>
+          </div>
+        </div>
+        <div className="field">
+        <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+            <h2>Data Science Kadr Yetişdirmə Proqramı</h2>
+            <p>Data Science-a giriş, Python, SQL, Data analitika, Machine Learning, Süni intellekt</p>
+          </div>
+        </div>
+        <div className="field">
+        <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+            <h2>IT Project Management kursu</h2>
+            <p>Agile, Waterfall metodları, Project structures, Soft skills, Risk analysis və s.</p>
+          </div>
+        </div>
+        <div className="field">
+        <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+            <h2>Product Owner kursu</h2>
+          </div>
+        </div>
+        <div className="field">
+        <div className="field-img">
+            <img src="/src/assets/house.png" alt="house" />
+          </div>
+          <div className="field-text">
+            <h2>IT Biznes Analitika kursları</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section1
