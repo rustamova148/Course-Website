369c99c8f7cd07f1c7cd6fe8a5c661d814c45f32
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2024-11-09 17:41:27 +0400

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Header />
       <Section1 />
+      <Section2 />
       <Footer />
     </div>
   )
---
+++ src/components/Section2/Section2.jsx
@@ -0,0 +1,36 @@
+import "./Section2.css";
+
+function Section2() {
+  return (
+    <div className="sec2">
+      <h1 className="sec2-header">Niyə biz?</h1>
+      <div className="sec2-box">
+        <div className="sec2-part1">
+          <div>
+            <img src="/src/assets/proqramlasdirma-kursu-24saat.png" />
+            <h5>24 saat açıq mərkəz</h5>
+          </div>
+          <div>
+            <img src="/src/assets/proqramlasdirma-kursu-1.png" />
+            <h5>Karyera mərkəzi</h5>
+          </div>
+          <div>
+            <img src="/src/assets/proqramlasdirma-kursu-2.png" />
+            <h5>Hər gün praktiki məşğələ</h5>
+          </div>
+          <div>
+            <img src="/src/assets/proqramlasdirma-kursu-3.png" />
+            <h5>Mentor dəstəyi</h5>
+          </div>
+        </div>
+        <div className="sec2-part2">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section2;
