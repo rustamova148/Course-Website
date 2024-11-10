92d295ec481785c96664bb2210b24ba490fbe255
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2024-11-10 16:53:32 +0400

    section2 added

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -3,6 +3,9 @@ import pk1 from '../../assets/proqramlasdirma-kursu-24saat.png'
 import pk2 from '../../assets/proqramlasdirma-kursu-1.png'
 import pk3 from '../../assets/proqramlasdirma-kursu-2.png'
 import pk4 from '../../assets/proqramlasdirma-kursu-3.png'
+import pk5 from '../../assets/proqramlasdirma-kursu-4.png'
+import pk6 from '../../assets/proqramlasdirma-kursu-5.png'
+import pk7 from '../../assets/proqramlasdirma-kursu-diploma1.png'
 
 function Section2() {
   return (
@@ -28,9 +31,18 @@ function Section2() {
           </div>
         </div>
         <div className="sec2-part2">
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <img src={pk5} />
+            <h5>İnternshiplərə qəbul</h5>
+          </div>
+          <div>
+            <img src={pk6} />
+            <h5>Bootcamp sistemi</h5>
+          </div>
+          <div>
+            <img src={pk7} />
+            <h5>Dərəcəli diplom və sertifikat</h5>
+          </div>
         </div>
       </div>
     </div>
